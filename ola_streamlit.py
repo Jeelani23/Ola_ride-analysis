0ae13462804a3518d30837d69cba1f49e16f8614
Jeelani23 <shaikakjeelani.1@gmail.com>
2025-05-22 21:51:52 +0530

    Update ola_streamlit.py

--- ola_streamlit.py
+++ ola_streamlit.py
@@ -35,6 +35,8 @@ if select == "🏠 HOME":
         st.link_button("Book Your Ride Now", "https://www.olacabs.com/")
 
     with col2:
+        st.header("Business inights")
+        #st.image(Image.open(r"C:\Users\INDIA\Downloads\ola-cabs-01.jpg"), width=600)
 elif select == "📊 Business Insights":
     st.subheader("📌 Select a query to analyze:")
 
